f6f04d56c7b19e3bea55c48227577c01cf548587
Rody Davis <rody.davis.jr@gmail.com>
2021-07-29 21:33:08 -0700

    adding 2 way sync

--- src/html-editor.ts
+++ src/html-editor.ts
@@ -1,12 +1,56 @@
 import { css, html, LitElement } from "lit";
 import { customElement } from "lit/decorators.js";
+import { createRef, Ref, ref } from "lit/directives/ref.js";
+import { CodeEditor } from "../third_party/lit-code-editor/src/code-editor";
+import { RichTextEditor } from "../third_party/lit-html-editor/src/rich-text-editor";
+
+import "../third_party/lit-code-editor/src/code-editor";
+import "../third_party/lit-html-editor/src/rich-text-editor";
 
 @customElement("html-editor")
 export class HTMLEditor extends LitElement {
-  static styles = css``;
+  private codeEditor: Ref<CodeEditor> = createRef();
+  private previewEditor: Ref<RichTextEditor> = createRef();
+  code: string = this.children[0].innerHTML.trim();
+
+  static styles = css`
+    main {
+      display: flex;
+      flex-direction: row;
+    }
+    code-editor {
+      width: 50%;
+    }
+    rich-text-editor {
+      width: 50%;
+      overflow-x: scroll;
+    }
+  `;
 
   render() {
-    return html``;
+    return html`
+      <main>
+        <code-editor
+          ${ref(this.codeEditor)}
+          language="html"
+          .code="${this.code}"
+          @change=${() => {
+            this.code = this.codeEditor.value!.getValue();
+            this.previewEditor.value!.setValue(this.code);
+          }}
+        ></code-editor>
+        <rich-text-editor
+          ${ref(this.previewEditor)}
+          content="${this.code}"
+          @focusout=${() => {
+            this.codeEditor.value!.setValue(this.code);
+          }}
+          @input=${() => {
+            this.code = this.previewEditor.value!.getValue();
+          }}
+        ></rich-text-editor>
+      </main>
+    `;
   }
 }
 
